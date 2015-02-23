60e606dcc99e3e86f9bd9ade945e1b27a25ba825
1303677 <1303677@RG42997.rgu.ac.uk>
2015-02-23 09:48:51 +0000

    CM2027 - Software Architecture and Software Engineering Week 4 - Agile Software Development
    Author: Scott Learmonth
    Date: 23/02/2015
    School: RGU

---
+++ src/lab4/Dictionary.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab4;
+
+/**
+ *
+ * @author 1303677
+ */
+public class Dictionary {
+    private String getDefinition;
+}
+
